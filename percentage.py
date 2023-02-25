cf7805c48058baa30f66a38747e3541f925a9d34
RALD <yr.alditya@gmail.com>
2023-02-25 12:28:16 +0700

    comment and percentage added

--- percentage.py
+++ percentage.py
@@ -17,7 +17,7 @@ total_progress = len(response.json())
 # KEMANA PERGINYA FILE INI
 path = "D:/PROJECT/Iseng2"
 filename = "pahlawan.csv"
-f = open(path+"/"+filename, 'w')
+f = open(path+"/"+filename, 'w', 0o777)
 
 # TULIS HEADER
 header = "LOAD DATA API PAHLAWAN INDONESIA"
@@ -28,9 +28,9 @@ for current_progress in range(total_progress + 1):
     # Hitung persentase
     percentage = current_progress/total_progress*100
     # Karena 100 persen = 50 tanda "." untuk progress bar, maka 1 "." = 2 persen
-    # Bulantkan ke bawah, karena ya persentase
+    # Bulantkan ke bawah, karena ya persentase, mau 79,8% tetep hitungannya 79%, berguna juga untuk memperjelas penentuan jumlah titik sebagai progress bar
     progress = math.floor(percentage)
-    # Tentukan jumlah titik sebagai progress bar
+    # Tentukan jumlah titik per 2 persen sebagai progress bar
     dot = int(progress/2) if progress % 2 == 0 else int((progress - 1)/2)
     # Tentukan sisanya, yaitu spasi = total bar (50) - progress bar
     space = bar - dot
